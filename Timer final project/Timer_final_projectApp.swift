0cea6481e87c86fb16d2f0a97442ed733eb80d63
Ariya C <acoleman@bsd220.org>
2023-12-11 09:27:59 -0600

    Initial Commit

---
+++ Timer final project/Timer_final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Timer_final_projectApp.swift
+//  Timer final project
+//
+//  Created by Ariya Coleman on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Timer_final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
